a3814f2a30ea43f65184c228526428fdbb1e774d
ibrahimYASAR <100451756+ibrahimYASAR@users.noreply.github.com>
2023-01-06 02:35:45 +0300

    Controller

--- src/main/java/kodlamaio/northwind/api/controlles/ProductsController.java
+++ src/main/java/kodlamaio/northwind/api/controlles/ProductsController.java
@@ -1,2 +1,62 @@
-package kodlamaio.northwind.api.controlles;public class ProductsController {
+package kodlamaio.northwind.api.controlles;
+
+import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utulities.results.DataResult;
+import kodlamaio.northwind.core.utulities.results.Result;
+import kodlamaio.northwind.entites.concretes.Product;
+import kodlamaio.northwind.entites.dtos.ProductWithCategoryDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+//hangi adreste yayınlayacağı==| /api/product adresine istek geldiğinde bu istegi
+  //--ProductsController karşılayacak
+public class ProductsController {
+
+    private ProductService productService;
+    @Autowired
+    public ProductsController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/getall")//path - yol
+    public DataResult<List<Product>> getAll(){
+        return this.productService.getAll();
+    }
+
+    @GetMapping("/getProductWithCategoryDetails")
+    public DataResult<List<ProductWithCategoryDto>> getProductWithCategoryDetails(){
+        return this.productService.getProductWithCategoryDetails();
+    }
+    @PostMapping("/add")
+    public Result add(@RequestBody Product product){
+        return this.productService.add(product);
+    }
+
+    @GetMapping("/getByProductName")
+    public DataResult<Product> getByProductName(@RequestParam String productName){
+        return this.productService.getByProductName(productName);
+    }
+
+    @GetMapping("/getByProductNameAndCategoryId")
+
+    public DataResult<Product> getByProductNameAndCategoryId
+            (@RequestParam("productName") String productName,@RequestParam("categoryId") int categoryId){
+        return this.productService.getByProductNameAndCategoryId(productName,categoryId);
+    }
+    @GetMapping("/getByProductNameContains")
+    public DataResult<List<Product>> getByProductNameContains(@RequestParam String productName){
+        return this.productService.getByProductNameContains(productName);
+    }
+    @GetMapping("/getAllByPage")//2
+    DataResult<List<Product>>  getAll(int pageNo,int pageSize){
+        return this.productService.getAll(pageNo,pageSize);
+    }
+    @GetMapping("/getAllDesc")//3
+    public DataResult<List<Product>> getAllSorted(){
+        return this.productService.getAllSorted();
+    }
 }
--- src/main/java/kodlamaio/northwind/api/controlles/UsersController.java
+++ src/main/java/kodlamaio/northwind/api/controlles/UsersController.java
@@ -1,2 +1,67 @@
-package kodlamaio.northwind.api.controlles;public class UsersController {
+package kodlamaio.northwind.api.controlles;
+
+import kodlamaio.northwind.business.abstracts.UserService;
+import kodlamaio.northwind.core.entities.User;
+import kodlamaio.northwind.core.utulities.results.DataResult;
+import kodlamaio.northwind.core.utulities.results.ErrorDataResult;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/api/users")
+public class UsersController {
+    private UserService userService;
+
+    @Autowired
+    public UsersController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @GetMapping("/getall")
+    public DataResult<List<User>> getAll(){
+        return this.userService.getAll();
+    }
+
+
+    @PostMapping(value = "/add")
+    public ResponseEntity<?> add(@Valid @RequestBody User user){
+        return ResponseEntity.ok(this.userService.add(user));
+    }
+    //ResponseEntity: 200-300-400-500 gibi hatalı veya olumlu dönmemize yarayan bir spring işlemi,
+        //-ok: ok işlemi ise olumluysa yap anlamına gelir.
+    //?: anlamı hatalı olursa 400,500 dön, olumluysa 200 dönmesine olanak sağlayan hangisi olursa,
+        //-esnek olarak onu çalıştırmasına yarayar.
+
+    public DataResult<User> findByEmail(String email){
+        return this.userService.findByEmail(email);
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDataResult<Object> handleValidationException
+            (MethodArgumentNotValidException exceptions){
+        Map<String,String> validationErrors=new HashMap<String,String>();
+        for (FieldError fieldError: exceptions.getBindingResult().getFieldErrors()){
+            validationErrors.put(fieldError.getField(),fieldError.getDefaultMessage());
+        }
+        ErrorDataResult<Object> errors=new ErrorDataResult<>(validationErrors
+                ,"Dogrulama hatalari");
+
+        return errors;
+    }
+
 }
