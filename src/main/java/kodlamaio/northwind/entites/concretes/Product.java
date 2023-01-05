1aadaa8fe52a3794e527752da54b537050c19926
ibrahimYASAR <100451756+ibrahimYASAR@users.noreply.github.com>
2023-01-06 02:35:21 +0300

    entity

--- src/main/java/kodlamaio/northwind/entites/concretes/Product.java
+++ src/main/java/kodlamaio/northwind/entites/concretes/Product.java
@@ -1,2 +1,41 @@
-package src.main.java.kodlamaio.northwind.entites.concretes;public class Product {
+package kodlamaio.northwind.entites.concretes;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Entity
+@Data
+@Table(name = "products")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private int id;
+
+   // @Column(name = "category_id")
+    //private int categoryId;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "unit_price")
+    private double unitPrice;
+
+    @Column(name = "units_in_stock")
+    private short unitsInStock;
+
+    @Column(name = "quantity_per_unit")
+    private String quantityPerUnit;
+
+    @ManyToOne()
+    @JoinColumn(name = "category_id")
+    private Category category;
 }
+
--- src/main/java/kodlamaio/northwind/entites/dtos/ProductWithCategoryDto.java
+++ src/main/java/kodlamaio/northwind/entites/dtos/ProductWithCategoryDto.java
@@ -1,2 +1,15 @@
-package kodlamaio.northwind.entites.dtos;public class ProductWithCategoryDto {
+package kodlamaio.northwind.entites.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductWithCategoryDto {
+    private int id;
+    private String productName;
+    private String categoryName;
+
 }
